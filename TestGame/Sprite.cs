33b481f0c4a9d9350e8ee558ac70248f796964f0
Sebastian Danyłko <danylko.sebastian@gmail.com>
2018-02-10 21:56:56 +0100

    base sprite code

---
+++ TestGame/Sprite.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace TestGame{
+
+    class Sprite{
+
+        private Vector3 spritePlacement;
+        private Vector3 spriteCollisionBoarders;
+        private int collisionOffset; 
+
+        public Vector3 SpritePlacement{
+
+            get{
+                return spritePlacement;
+            }                          
+
+            set{
+                spritePlacement = value;
+            }
+        }
+
+        public Vector3 SpriteCollisionBoarders{
+
+            get{
+                return spriteCollisionBoarders;
+            }
+
+            set{
+                spriteCollisionBoarders = value;
+            }
+        }
+
+        public int CollisionOffset {
+            get {
+                return collisionOffset;
+            }
+
+            set {
+                collisionOffset = value;
+            }
+        }
+
+        public Sprite(int sprite) {
+        }
+
+    }
+}
